e5808f8f84941bf29b506334a7f84bd9dab6db28
TheChiragRathod <crathod116@rku.ac.in>
2020-10-19 22:06:47 +0530

    added method btnCall for calling.

--- app/src/main/java/com/chirag/tutorial13/MainActivity.java
+++ app/src/main/java/com/chirag/tutorial13/MainActivity.java
@@ -1,10 +1,17 @@
 package com.chirag.tutorial13;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +31,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnCall(View view)
     {
-
+        if(isCallPermissionAllowed())
+        {
+            doCall();
+        }
     }
 
     public void btnMessage(View view)
     {
 
+    }
+
+    private boolean isCallPermissionAllowed()
+    {
+        if(Build.VERSION.SDK_INT>=23)
+        {
+            if(checkSelfPermission(Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED)
+            {
+                return true;
+            }
+            else {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE},REQUESTCODE_CALL);
+                return false;
+            }
+        }
+        else
+        {
+            //permission is automatically granted on sdk<23 upon installation
+            return true;
+        }
+    }
+    protected void doCall()
+    {
+        try
+        {
+            String number = editTextNumber.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + number));
+            startActivity(intent);
+        }
+        catch (Exception objExc)
+        {
+            Toast.makeText(this, objExc.getMessage().toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
