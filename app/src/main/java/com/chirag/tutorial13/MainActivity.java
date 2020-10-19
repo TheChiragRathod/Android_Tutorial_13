31ae240524908c36db811058fe41e1676aac1e98
TheChiragRathod <crathod116@rku.ac.in>
2020-10-19 22:01:38 +0530

    added two buttons onClick Methods.

--- app/src/main/java/com/chirag/tutorial13/MainActivity.java
+++ app/src/main/java/com/chirag/tutorial13/MainActivity.java
@@ -3,12 +3,30 @@ package com.chirag.tutorial13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextNumber;
+    EditText editTextMessage;
+
+    protected static final int REQUESTCODE_CALL=1;
+    protected static final int REQUESTCODE_SMS=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnCall(View view)
+    {
+
+    }
+
+    public void btnMessage(View view)
+    {
+
     }
 }
