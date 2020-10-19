328ddaba645deb0273b4f4189553fdf1bb8f4998
TheChiragRathod <crathod116@rku.ac.in>
2020-10-19 22:03:49 +0530

    added two permisions called CALL_PHONE and SEND_SMS in AndroidManifest.xml file

--- app/src/main/java/com/chirag/tutorial13/MainActivity.java
+++ app/src/main/java/com/chirag/tutorial13/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNumber=findViewById(R.id.edtNumber);
+        editTextMessage=findViewById(R.id.edtMessage);
 
     }
 
